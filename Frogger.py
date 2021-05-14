880be34621878d3441c9787c97890c884dcbe19f
mikeNegron <mikenegron14@gmail.com>
2021-05-14 13:23:08 -0400

    BrainStorm

--- Frogger.py
+++ Frogger.py
@@ -1,1 +1,17 @@
-"""Testing if repository was made correctly."""
+from graphics import*
+
+#HitBox class
+
+#Car class to inherit HitBox class (bool expression to determine if it'll kill or not)
+    #4 types of cars
+        #Could be implemented on a list
+
+#Snake class to inherit HitBox class
+
+#Turtle class to control their behavior (sink and float) inherits HitBox too
+
+#Frog class
+
+#Reward class
+
+#Game class to run frogger
