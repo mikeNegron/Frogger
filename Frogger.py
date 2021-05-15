c18129ab9b889184632744d8efc7ddecc301c2ff
mikeNegron <mikenegron14@gmail.com>
2021-05-14 20:05:52 -0400

    Display commit

--- Frogger.py
+++ Frogger.py
@@ -47,9 +47,84 @@ class HitBox:
         self.corner_coords = self.get_coords()
         self.side_coords = self.map_sides()
 
+class Display:
+    def __init__(self, width, height, window):
+        self.width = width
+        self.height = height
 
+        self.window = window
+
+        self.starts = {}
+        self.river = {}
+        self.road_lines = {}
+
+    def generate_starts(self, start_amount):
+        self.starts[0] = Rectangle(Point(0, 550), Point(self.width, self.height))
+        self.starts[0].setFill('Green')
+        self.starts[0].setOutline('Green')
+        self.starts[0].draw(self.window)
+
+        for i in range(1, start_amount):
+            self.starts[i] = self.starts[i - 1].clone()
+            
+            self.starts[i].move(0, -250)
+            
+            self.starts[i].draw(self.window)
+
+    def generate_river(self, goal_amount):
+        self.river[0] = Rectangle(Point(0, -25), Point(100, 75))
+        self.river[0].setFill('Green')
+        self.river[0].setOutline('Green')
+        self.river[0].draw(self.window)
+
+        for i in range(1, goal_amount):
+            self.river[i] = self.river[i - 1].clone()
+            self.river[i].move(100, 0)
+
+            if not(i%2 == 0):
+                self.river[i].setFill('Blue')
+                self.river[i].setOutline('Blue')
+
+            else:
+                self.river[i].setFill('Green')
+                self.river[i].setOutline('Green')
+
+            self.river[i].draw(self.window)
+
+        water_body = Rectangle(Point(0, 75), Point(self.width, self.height/2))
+        water_body.setFill('Blue')
+        water_body.setOutline('Blue')
+        water_body.draw(self.window)
+        
+    def generate_lines(self):
+        self.road_lines[0] = Rectangle(Point(0, 435), Point(50, 455))
+        self.road_lines[0].setFill('Yellow')
+        self.road_lines[0].draw(self.window)
+
+        for j in range(1, 9):
+            self.road_lines[j] = self.road_lines[j - 1].clone()
+            self.road_lines[j].move(125, 0)
+            self.road_lines[j].draw(self.window)
+
+    def generate_field(self):
+        self.generate_starts(2)
+        self.generate_river(11)
+        self.generate_lines()
         
 
+def main():
+    win = GraphWin('Frogger', 900, 600, autoflush=False)
+    win.setBackground('Black')
+
+    ui = Display(900, 600, win)
+    ui.generate_field()
+
+    win.getMouse()
+    win.close()
+
+
+if __name__ == '__main__':
+    main()
 
 #Car class to inherit HitBox class (bool expression to determine if it'll kill or not)
     #4 types of cars
@@ -64,25 +139,3 @@ class HitBox:
 #Reward class
 
 #Game class to run frogger
-
-def main():
-    win = GraphWin('temp', 100, 100, autoflush=False)
-
-    s = HitBox(60, 20, 80, 40)
-
-    s.box.setOutline('Black')
-    s.box.setWidth(4)
-    s.box.draw(win)
-
-    for i in range(60):
-        s.move_box(1)
-
-        update(15)
-
-    win.getMouse()
-    win.close()
-
-
-    
-if __name__ == '__main__':
-    main()
